dcee7be6a912ea8b3ccce3b5032cb48506c9c43e
Ксения <urukinaksusha@gmail.com>
2023-03-20 20:39:51 +0600

    add 3

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ repositories {
 val serializationVersion = "1.5.0-RC"
 val kotestVersion = "5.4.1"
 dependencies {
+    implementation("org.scalaz:scalaz-deriving-jsonformat_2.12:1.0.0")
     testImplementation("io.kotest:kotest-runner-junit5-jvm:$kotestVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$serializationVersion")
 }
--- src/main/kotlin/lab_2/Main.kt
+++ src/main/kotlin/lab_2/Main.kt
@@ -1,18 +1,15 @@
 package lab_2
 
-//import kotlinx.serialization.*
-import kotlinx.serialization.KSerializer
-import kotlinx.serialization.MissingFieldException
+import kotlinx.serialization.*
 import kotlinx.serialization.json.Json
-import kotlinx.serialization.Serializable
 import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.descriptors.PrimitiveKind
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
+import java.awt.datatransfer.StringSelection
 import java.text.SimpleDateFormat
 import java.util.*
-import kotlinx.serialization.SerialName
 
 object ObjectMapper {
     val dateFormat = "yyyy.MM.dd"
@@ -31,7 +28,8 @@ object DateAsLongSerializer : KSerializer<Date> {
 
 @Serializable
 data class Lesson<Date>(val name: String,
-                        @Serializable(with = DateAsLongSerializer::class)
+//                        @Serializable(with = DateAsLongSerializer::class)
+//                        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
                         val date: Date
 )
 
@@ -51,44 +49,66 @@ data class Person(
     )
 }
 
+//@Serializable(WeekTypeSerializer::class)
+enum class WeekType { TRAINING, SESSION, HOLIDAY }
+class Week(val number: Int, val type: WeekType)
 
-
-
+//object WeekTypeSerializer : KSerializer<WeekType> {
+//    override val descriptor = SerialDescriptor("Week", StringSelection().descriptor)
+//
+//}
 
 //@OptIn(ExperimentalSerializationApi::class)
 fun main() {
     // 1 ПУНКТ
     val dateObject = SimpleDateFormat(ObjectMapper.dateFormat)
+//    println(dateObject.toPattern()) // yyyy.MM.dd
     val date = dateObject.format(Date())
 //    println(date) // 2023.03.19
 
-//    val lesson = Lesson("Java Date", Date(12))
-//    val lessonJson = Json.encodeToString(Lesson.serializer(DateAsLongSerializer), lesson)
-//
-//    println(lessonJson) // {"name":"Java Date","date":12}
+    val lesson = Lesson("Java Date", date)
+    val lessonJson = Json.encodeToString(/*Lesson.serializer(DateAsLongSerializer),*/ lesson)
+
+    println(lessonJson) // {"name":"Java Date","date":12}
+
+
 
     // 2 ПУНКТ
 
-    val course = Course("Math", Person("Leonard Euler"))
-    val courseJson = Json.encodeToString(Course.serializer(), course)
-
-    println(courseJson) // {"name":"Math","tutor":{"firstname":"Leonard","surname":"Euler"}}
-
-    val string = "{\"name\": \"Phys\"}"
-    println(string)
-
-    /* null-типы обрабатываются только в том случае,
-    если есть значение по-умолчанию, иначе выбросится исключение.*/
-    try {
-        val stringJson = Json.decodeFromString(Course.serializer(), string)
-        println(stringJson.name)
-        println(stringJson.person)
-    } catch (e: MissingFieldException) {
-        println("ERROR!")
-    }
+//    val course = Course("Math", Person("Leonard Euler"))
+//    val courseJson = Json.encodeToString(Course.serializer(), course)
+//
+//    println(courseJson) // {"name":"Math","tutor":{"firstname":"Leonard","surname":"Euler"}}
+//
+//    val string = "{\"name\": \"Phys\"}"
+//    println(string)
+//
+//    /* null-типы обрабатываются только в том случае,
+//    если есть значение по-умолчанию, иначе выбросится исключение.*/
+//    try {
+//        val stringJson = Json.decodeFromString(Course.serializer(), string)
+//        println(stringJson.name)
+//        println(stringJson.person)
+//    } catch (e: MissingFieldException) {
+//        println("ERROR!")
+//    }
 
 
     // 3 ПУНКТ
+
+    val weekArray = arrayListOf(
+            1 to WeekType.TRAINING,
+            2 to WeekType.TRAINING,
+            3 to WeekType.SESSION,
+            4 to WeekType.HOLIDAY
+    )
+    val weekArrayJson = Json.encodeToString(weekArray)
+    println(weekArrayJson) // [{"first":1,"second":"TRAINING"},{"first":2,"second":"TRAINING"},{"first":3,"second":"SESSION"},{"first":4,"second":"HOLIDAY"}]
+
+    val weeks = weekArray.map { Week(it.first, it.second) }
+//    println(weeks)
+    val weeksJson = Json.encodeToString(weeks)
+    println(weeksJson)
 
 
 }
