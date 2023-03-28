054ffbd59bfdeb5f8207b4f78d28dce481425662
Ксения <urukinaksusha@gmail.com>
2023-03-28 21:07:46 +0600

    final

--- src/main/kotlin/lab_2/Main.kt
+++ src/main/kotlin/lab_2/Main.kt
@@ -1,55 +1,60 @@
 package lab_2
 
 import com.fasterxml.jackson.annotation.JsonFormat
-import kotlinx.serialization.KSerializer
-import kotlinx.serialization.MissingFieldException
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
+import kotlinx.serialization.*
 import kotlinx.serialization.descriptors.PrimitiveKind
 import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 import kotlinx.serialization.json.Json
+//import kotlinx.serialization.json.Json.Default.encodeToString
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import java.text.SimpleDateFormat
 import java.util.*
 
 
 class Lesson(
-    val name: String,
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd.MM.yyyy")
-    val date: Date
+        val name: String,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd.MM.yyyy")
+        val date: Date
 )
 
 
 @Serializable
-data class Course(val name: String, @SerialName("tutor") val person: Person?)  /*= Person("A B")*/
+data class Course(
+        val name: String,
+        @SerialName("tutor") val person: Person? /*= Person("A B")*/
+)
 
 @Serializable
 data class Person(
-    val firstname: String,
-    val surname: String
+        val firstname: String,
+        val surname: String
 ) {
     constructor(name: String) : this(
-        name.substringBefore(" "),
-        name.substringAfter(" ")
+            name.substringBefore(" "),
+            name.substringAfter(" ")
     )
 }
 
 enum class WeekType { TRAINING, SESSION, HOLIDAY }
 
 @Serializable
-class Week(val number: Int, val type: WeekType)
+data class Week(val number: Int,
+           @Serializable(with = WeekTypeSerializer::class)
+           val type: WeekType)
 
 object WeekTypeSerializer : KSerializer<WeekType> {
-    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("WeekType", PrimitiveKind.STRING)
-
+    override val descriptor: SerialDescriptor =
+            PrimitiveSerialDescriptor("WeekType", PrimitiveKind.STRING)
     override fun serialize(encoder: Encoder, value: WeekType) {
         encoder.encodeString(
-            when (value) {
-                WeekType.TRAINING -> "Обучение"
-                WeekType.SESSION -> "Сессия"
-                WeekType.HOLIDAY -> "Каникулы"
-            }
+                when (value) {
+                    WeekType.TRAINING -> "Обучение"
+                    WeekType.SESSION -> "Сессия"
+                    WeekType.HOLIDAY -> "Каникулы"
+                }
         )
     }
 
@@ -63,22 +68,19 @@ object WeekTypeSerializer : KSerializer<WeekType> {
     }
 }
 
-@Serializable
-data class WeekSerialize(val number: Int, @Serializable(with = WeekTypeSerializer::class) val type: WeekType)
-
 
 fun main() {
     // 1 ПУНКТ
 
-//    val objectMapper = jacksonObjectMapper()
-//    objectMapper.dateFormat = SimpleDateFormat("yyyy.MM.dd")
-//
-//    val lesson = Lesson("Java Date", Date())
-//    val lessonJson = objectMapper.writeValueAsString(lesson)
-//
-//    // При одновременном использовании двух паттернов,
-//    // на выходе будет формат из аннотации, указаной в классе Lesson.
-//    println(lessonJson)
+    val objectMapper = jacksonObjectMapper()
+    objectMapper.dateFormat = SimpleDateFormat("yyyy.MM.dd")
+
+    val lesson = Lesson("Java Date", Date())
+    val lessonJson = objectMapper.writeValueAsString(lesson)
+
+    // При одновременном использовании двух паттернов,
+    // на выходе будет формат из аннотации, указаной в классе Lesson.
+    println(lessonJson)
 
     // 2 ПУНКТ
 
@@ -86,6 +88,7 @@ fun main() {
     val courseJson = Json.encodeToString(Course.serializer(), course)
 
     println(courseJson) // {"name":"Math","tutor":{"firstname":"Leonard","surname":"Euler"}}
+
 
     val string = "{\"name\": \"Phys\"}"
     println(string)
@@ -99,27 +102,25 @@ fun main() {
     } catch (e: MissingFieldException) { // Исключение отсутствующего поля
         println("ERROR!")
     }
-//
-//
-//    // 3 ПУНКТ
-//
-//    val weekArray = arrayListOf(
-//            1 to WeekType.TRAINING,
-//            2 to WeekType.TRAINING,
-//            3 to WeekType.SESSION,
-//            4 to WeekType.HOLIDAY
-//    )
-//    val weekArrayJson = Json.encodeToString(weekArray)
-//    println(weekArrayJson) // [{"first":1,"second":"TRAINING"},{"first":2,"second":"TRAINING"},{"first":3,"second":"SESSION"},{"first":4,"second":"HOLIDAY"}]
-//
-//    val weeks = weekArray.map { Week(it.first, it.second) }
-////    println(weeks)
-//    val weeksJson = Json.encodeToString(weeks)
-//    println(weeksJson)
-//
-//
-//    val serWeeks = weekArray.map { WeekSerialize(it.first, it.second) }
-//    val serWeeksJson = Json.encodeToString(serWeeks)
-//    println(serWeeksJson)
+
+    // 3 ПУНКТ
+
+
+    val weekArray = arrayListOf(
+            1 to WeekType.TRAINING,
+            2 to WeekType.TRAINING,
+            3 to WeekType.SESSION,
+            4 to WeekType.HOLIDAY
+    )
+    val weekArrayJson = Json.encodeToString(weekArray)
+    println(weekArrayJson)
+
+    val weeks = weekArray.map { Week(it.first, it.second) }
+    val weeksJson = Json.encodeToString(weeks)
+    println(weeksJson)
+
+    val serWeeks = weekArray.map { Week(it.first, it.second) }
+    val serWeeksJson = Json.encodeToString(serWeeks)
+    println(serWeeksJson)
 
 }
